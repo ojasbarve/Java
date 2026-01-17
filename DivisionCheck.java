d1f31b11d1255f03a965e1b43004e6c92a8fb7ac
Ojas Barve <ojasb@Ojass-MacBook-Pro.local>
2026-01-18 00:22:41 +0530

    added

---
+++ DivisionCheck.java
@@ -0,0 +1,13 @@
+public class DivisionCheck {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+
+        if (b == 0) {
+            System.out.println("Error: Division by zero is not allowed");
+        } else {
+            int result = a / b;
+            System.out.println("Result: " + result);
+        }
+    }
+}
