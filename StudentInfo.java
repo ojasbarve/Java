ed903a73069a06050fd4c19e29b1b7dbb6b6facb
Ojas Barve <ojasb@Ojass-MacBook-Pro.local>
2026-01-18 00:14:48 +0530

    added student

---
+++ StudentInfo.java
@@ -0,0 +1,7 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Name: Ojas Barve");
+        System.out.println("Roll No: 23");
+        System.out.println("Address: Pune, Maharashtra");
+    }
+}
